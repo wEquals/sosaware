81b8bf2294f007975804ce31d47a997c2b72f803
wo6zy <102326511+wEquals@users.noreply.github.com>
2022-12-17 14:39:58 +0000

    Create auth.lua

---
+++ auth.lua
@@ -0,0 +1,85 @@
+--dont obfascate the license cause they need to put it in there
+local License = "LKYNGS-QA6DWG-PB2Z40-VCJH96-TDL85J-4EB73W" --* Your License to use this script.
+
+
+--obfascate this bit downwards
+local HttpService = game:GetService("HttpService")
+local StarterGui = game:GetService("StarterGui")
+local LuaName = "sosaware"
+
+StarterGui:SetCore("SendNotification", {
+	Title = LuaName,
+	Text = "Intializing Authentication...",
+	Duration = 5
+})
+
+--* Configuration *--
+local initialized = false
+local sessionid = ""
+
+
+--* Application Details *--
+Name = "sosaware" --* Application Name
+Ownerid = "TkpvYvJCNM" --* OwnerID
+APPVersion = "1.0" --* Application Version
+
+local req = game:HttpGet('https://keyauth.win/api/1.1/?name=' .. Name .. '&ownerid=' .. Ownerid .. '&type=init&ver=' .. APPVersion)
+
+if req == "KeyAuth_Invalid" then 
+   print(" Error: Application not found.")
+
+   StarterGui:SetCore("SendNotification", {
+	   Title = LuaName,
+	   Text = " Error: Application not found.",
+	   Duration = 3
+   })
+
+   return false
+end
+
+local data = HttpService:JSONDecode(req)
+
+if data.success == true then
+   initialized = true
+   sessionid = data.sessionid
+   --print(req)
+elseif (data.message == "invalidver") then
+   print(" Error: Wrong application version..")
+
+   StarterGui:SetCore("SendNotification", {
+	   Title = LuaName,
+	   Text = " Error: Wrong application version..",
+	   Duration = 3
+   })
+
+   return false
+else
+   print(" Error: " .. data.message)
+   return false
+end
+
+print("\n\n Licensing... \n")
+local req = game:HttpGet('https://keyauth.win/api/1.1/?name=' .. Name .. '&ownerid=' .. Ownerid .. '&type=license&key=' .. License ..'&ver=' .. APPVersion .. '&sessionid=' .. sessionid)
+local data = HttpService:JSONDecode(req)
+
+
+if data.success == false then 
+    StarterGui:SetCore("SendNotification", {
+	    Title = LuaName,
+	    Text = " Error: " .. data.message,
+	    Duration = 5
+    })
+
+    return false
+end
+
+StarterGui:SetCore("SendNotification", {
+	Title = LuaName,
+	Text = "Loading sosaware",
+	Duration = 5
+})
+
+
+--* Your code here *--
+
+loadstring(game:HttpGet'https://raw.githubusercontent.com/wEquals/sosaware/main/main.lua')()
